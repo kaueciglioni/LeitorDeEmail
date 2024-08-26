f6a66f05aef3fb80d50f50242e9437f674bbbd99
Kauê <Kau@DESKTOP-TMCM2QH>
2024-08-26 16:05:25 -0300

    Adicionar arquivos de projeto.

---
+++ FileMailInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LeitorDeEmail
+{
+
+    public class FileMailInfo
+    {
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+        public string Date { get; set; }
+        public string ExistingAttachment { get; set; }
+        public string MessageId { get; set; }
+    }
+}
+
